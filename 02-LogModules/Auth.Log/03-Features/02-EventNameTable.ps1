345fbb3b9a387cdf84eb0d34e488256538d580f5
Eilay Yosfan (DFIR) <132997318+YosfanEilay@users.noreply.github.com>
2024-03-23 14:21:45 +0200

    Update 02-EventNameTable.ps1

--- 02-LogModules/Auth.Log/03-Features/02-EventNameTable.ps1
+++ 02-LogModules/Auth.Log/03-Features/02-EventNameTable.ps1
@@ -1,36 +1,30 @@
-# hashtable to store the 5th word
+# Hashtable to store the 5th word
 $5th_word_table = @{}
 
-# variable to get auth.log copy content.
-$AuthLogCopyContent = Get-Content "$RunningPath\02-LogModules\Auth.Log\01-LogCopy\Auth.Log.Parser.Copy.txt"
+# Regular expression pattern to match the fifth word
+$pattern = '\S+\s+\S+\s+\S+\s+\S+\s+(\S+)'
 
-# foreach loop to iterate through lines of the auth.log file.
-foreach ($SingleLine in $AuthLogCopyContent) {
+# Get the content of the file directly using switch statement
+switch -Regex -File "$RunningPath\02-LogModules\Auth.Log\01-LogCopy\Auth.Log.Parser.Copy.txt" {
+    $pattern {
+        $5th_word = $matches[1] -replace '\[|[0-9]{1,15}|\]|\:',''
 
-  # Split each line into words
-  $Words = $SingleLine -split ' '
-
-  # variable to remove irrelevant characters from the 5th word 
-  $5th_word = $words[4] -replace '\[|[0-9]{1,15}|\]|\:',''
-
-  if ($5th_word_table.ContainsKey($5th_word)) {
-    $5th_word_table[$5th_word]++
-  }
-
-  else {
-    $5th_word_table[$5th_word] = 1
-  }
-
-}
-
-# change the property name of the Keys and Values of $5th_word_table
-$5th_word_table_Fixed = foreach ($entry in $5th_word_table.GetEnumerator() | Sort-Object Value -Descending) {
-    [pscustomobject]@{
-        "Event Name" = $entry.Key
-        "Count" = $entry.Value
+        if ($5th_word_table.ContainsKey($5th_word)) {
+            $5th_word_table[$5th_word]++
+        } else {
+            $5th_word_table[$5th_word] = 1
+        }
     }
 }
 
-# Explicitly set the property names for consistent output
+# Transform the hashtable into an array of custom objects for easier formatting
+$5th_word_table_Fixed = $5th_word_table.GetEnumerator() | Sort-Object Value -Descending | ForEach-Object {
+    [pscustomobject]@{
+        "Event Name" = $_.Key
+        "Count"      = $_.Value
+    }
+}
+
+# Output the result
 Write-Output ""
 $5th_word_table_Fixed | Format-Table -Property "Event Name", "Count" | Out-String -Width 50 | ForEach-Object { $_.Trim() }
