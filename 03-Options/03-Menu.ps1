e9c9d6a09a96742476624b0593f2380fa7efc983
Eilay Yosfan (DFIR) <132997318+YosfanEilay@users.noreply.github.com>
2024-04-14 15:20:04 +0300

    changee

--- 03-Options/03-Menu.ps1
+++ 03-Options/03-Menu.ps1
@@ -1,12 +1,4 @@
-<<<<<<< Updated upstream
-Write-Output "┌> How To Run Example: MasterParser.ps1 -O Start"
-=======
-<<<<<<< HEAD
 ﻿Write-Output "┌> How To Run Example: MasterParser.ps1 -O Start"
-=======
-Write-Output "┌> How To Run Example: MasterParser.ps1 -O Start"
->>>>>>> 56e44ccb3c15a8e7ece5a6596a96fadb299359b5
->>>>>>> Stashed changes
 Write-Output "├~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~╮"
 Write-Output "│                               Options (-O)                               │"
 Write-Output "├~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~┤"
@@ -20,11 +12,4 @@ Write-Output "│                                                               
 Write-Output "│4. Purge        Purge MasterParser tool footprint from this host          │"
 Write-Output "│                                                                          │"
 Write-Output "╰~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~╯"
-<<<<<<< HEAD
 Write-Output ""
-<<<<<<< Updated upstream
-=======
-=======
-Write-Output ""
->>>>>>> 56e44ccb3c15a8e7ece5a6596a96fadb299359b5
->>>>>>> Stashed changes
